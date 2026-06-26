312cd4a09af8d2e9cee1039b222cf1c03a28269a
maxx52 <gospelwork@yandex.ru>
2026-06-26 17:51:15 +0700

    Added JsonSettingsRepository.kt

---
+++ desktopApp/src/main/kotlin/holypresenter/org/platform/settings/repository/JsonSettingsRepository.kt
@@ -0,0 +1,36 @@
+package holypresenter.org.platform.settings.repository
+
+import holypresenter.org.platform.settings.AppSettings
+import holypresenter.org.platform.settings.SettingsRepository
+import kotlinx.serialization.json.Json
+import java.io.File
+
+class JsonSettingsRepository(
+    private val settingsFile: File
+) : SettingsRepository {
+
+    private val json = Json {
+        prettyPrint = true
+        ignoreUnknownKeys = true
+    }
+
+    init {
+        settingsFile.parentFile?.mkdirs()
+    }
+
+    override fun load(): AppSettings {
+        if (!settingsFile.exists()) {
+            return AppSettings()
+        }
+
+        return json.decodeFromString<AppSettings>(
+            settingsFile.readText()
+        )
+    }
+
+    override fun save(settings: AppSettings) {
+        settingsFile.writeText(
+            json.encodeToString(settings)
+        )
+    }
+}
