c8d7f72c31384456b7b7d2fee6f7fefb07447d35
maxx52 <gospelwork@yandex.ru>
2026-06-28 05:42:18 +0700

    Publish platform API to Maven local

--- platform-api/build.gradle.kts
+++ platform-api/build.gradle.kts
@@ -2,8 +2,14 @@ plugins {
     alias(libs.plugins.kotlinJvm)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
+
+    `maven-publish`
 }
+
+group = "org.holypresenter"
+version = "0.1.0"
 
 dependencies {
     implementation("org.jetbrains.compose.runtime:runtime:1.11.1")
+    implementation("org.jetbrains.compose.material3:material3:1.9.0")
 }
