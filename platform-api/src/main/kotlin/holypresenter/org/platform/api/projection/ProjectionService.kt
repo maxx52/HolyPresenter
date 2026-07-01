db4a69149f85b6c7cfdf499327515f7348784a67
maxx52 <gospelwork@yandex.ru>
2026-07-01 21:36:51 +0700

    Add presentation test module

---
+++ platform-api/src/main/kotlin/holypresenter/org/platform/api/projection/ProjectionService.kt
@@ -0,0 +1,16 @@
+package holypresenter.org.platform.api.projection
+
+import holypresenter.org.platform.api.presentation.Presentation
+import holypresenter.org.platform.api.presentation.PresentationSlide
+
+interface ProjectionService {
+    val currentPresentation: Presentation?
+    val currentSlideIndex: Int
+    val currentSlide: PresentationSlide?
+
+    fun present(presentation: Presentation)
+    fun next()
+    fun previous()
+    fun goTo(index: Int)
+    fun clear()
+}
